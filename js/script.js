fd9af58914e3ff7898e7e9ec941066fb0fbc94e5
Mattia <matpolli93@gmail.com>
2021-03-25 15:32:41 +0100

    Add style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const app = new Vue(
+    {
+        el: "#app",
+        data: {
+            images: [
+                "https://wallpapercave.com/wp/3jHyPCx.jpg",
+                "https://wallpapercave.com/wp/wp8727056.jpg",
+                "https://wallpapercave.com/uwp/uwp112289.jpeg",
+                "https://wallpapercave.com/wp/wp5226887.jpg",
+                "https://wallpapercave.com/uwp/uwp118964.jpeg"
+            ],
+        },
+        methods: {
+            
+        }
+    }
+);
