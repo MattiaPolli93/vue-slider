af66dd8687e4301153172f691ebea4dee84a89f4
Mattia <matpolli93@gmail.com>
2021-03-26 13:21:59 +0100

    Add more functionalities (autoplay, keyboard events, taglines)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,49 @@
 "use strict";
 
+// Create a Slider
+
 const app = new Vue(
     {
         el: "#app",
         data: {
             images: [
-                "https://wallpapercave.com/wp/3jHyPCx.jpg",
-                "https://wallpapercave.com/wp/wp8727056.jpg",
-                "https://wallpapercave.com/uwp/uwp112289.jpeg",
-                "https://wallpapercave.com/wp/wp5226887.jpg",
-                "https://wallpapercave.com/uwp/uwp118964.jpeg"
+                {
+                    image: "https://wallpapercave.com/wp/3jHyPCx.jpg",
+                    name: "New York City"
+                },
+                {
+                    image: "https://wallpapercave.com/wp/wp8727056.jpg",
+                    name: "Rome"
+                },
+                {
+                    image: "https://wallpapercave.com/wp/wp7868527.jpg",
+                    name: "London"
+                },
+                {
+                    image: "https://wallpapercave.com/wp/wp5226887.jpg",
+                    name: "Moscow"
+                },
+                {
+                    image: "https://wallpapercave.com/uwp/uwp118964.jpeg",
+                    name: "Sydney"
+                }
             ],
             currentIndex: 0,
+            autoplay: null
+        },
+        mounted: function() {
+            this.autoplay = setInterval(this.next, 2500);
+
+            const component = this;
+
+            window.addEventListener("keydown", function(event) {
+                
+                if (event.which == 37 || event.which == 8) {
+                    component.clickPrev();
+                } else if (event.which == 39 || event.which == 13) {
+                    component.clickNext();
+                }
+            });
         },
         methods: {
             next: function() {
@@ -27,6 +59,14 @@ const app = new Vue(
                 if (this.currentIndex < 0) {
                     this.currentIndex = this.images.length - 1;
                 }
+            },
+            clickNext: function() {
+                this.next();
+                clearInterval(this.autoplay);
+            },
+            clickPrev: function() {
+                this.prev();
+                clearInterval(this.autoplay);
             }
         }
     }
